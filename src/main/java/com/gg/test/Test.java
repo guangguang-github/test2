2584d24db9fd896a927698173ef4dc622d63d6a3
guangguang <szby1023@163.com>
2020-09-12 17:18:01 +0800

    提交到本地仓库01

---
+++ src/main/java/com/gg/test/Test.java
@@ -0,0 +1,9 @@
+package com.gg.test;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello world Github!!");
+        int abc = 123;
+        System.out.println(abc);
+    }
+}
